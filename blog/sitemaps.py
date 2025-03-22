1e4f84909a38ce54bc7f1a820922a615a0752891
juaninga <juan.inga@unae.edu.ec>
2025-03-22 11:58:31 -0500

    sitemaps

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -1,17 +1,38 @@
 from django.contrib import sitemaps
 from django.urls import reverse
 
-from .models import Post
+from .models import Post, Categoria
+
+class PostsSitemap(sitemaps.Sitemap):
+    priority = 0.9
+    changefreq = 'weekly'
+
+    def items(self):
+        return [
+            'blog:posts'
+        ]
+
+    def location(self, item):
+        return reverse(item)
+    
+
+class PostsCategoriaSitemap(sitemaps.Sitemap):
+    priority = 0.8
+    changefreq = 'weekly'
+
+    def items(self):
+        return Categoria.objects.all()
+
+    def location(self, obj):
+        return reverse('blog:posts_categoria', kwargs={'categoria': obj.slug})
+
 
 class PostSitemap(sitemaps.Sitemap):
-    changefreq = "never"
+    changefreq = "weekly"
     priority = 0.9
 
     def items(self):
         return Post.objects.filter(activo=True)
-
-    # def lastmod(self, obj):
-    #     return obj.modified_by
 
     def location(self, obj):
         return reverse('blog:post', args=[obj.slug])
