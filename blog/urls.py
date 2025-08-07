a8d9a9ea61f3711689a93a3a0a73a2474c188183
Juan Inga <juan.ingaor@gmail.com>
2025-08-07 14:55:06 -0500

    [CHANGE] Update URL patterns to use dynamic category path from settings; bump version to 0.1.19 in setup.py

--- blog/urls.py
+++ blog/urls.py
@@ -7,9 +7,10 @@ app_name = 'blog'
 
 base_path_post = getattr(settings, 'BLOG_PATH_POST', 'post')
 base_path_list_posts = getattr(settings, 'BLOG_PATH_LIST_POSTS', 'posts')
+base_path_category = getattr(settings, 'BLOG_PATH_CATEGORY', 'categoria')
 
 urlpatterns = [
     path(f'{base_path_post}/<str:slug>/', PostView.as_view(), name='post'),
-    path('categoria/<slug:categoria>/', PostsCategoriaListView.as_view(), name='posts_categoria'),
+    path(f'{base_path_category}/<slug:categoria>/', PostsCategoriaListView.as_view(), name='posts_categoria'),
     path(f'{base_path_list_posts}/', PostsListView.as_view(), name='posts'),
 ]
